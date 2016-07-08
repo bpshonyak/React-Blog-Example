cd2a07d5f339db454975f5633298e9cafece5bb8
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-09 02:18:27 +0900

    added action for create post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
 
 const ROOT_URL= 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=sb39n2u49y3284sihadfg8232';
@@ -13,4 +14,14 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: REQUEST
   };
+}
+
+export function createPost(props) {
+
+  const REQUEST = axios.get(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: REQUEST
+  }
 }
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostNew extends Component {
 
   render() {
+
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>New Post Form</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create A New Post</h3>
+
+        <div className='form-group'>
+          <label>Title</label>
+          <input type='text' className='form-control' {...title} />
+        </div>
+
+        <div className='form-group'>
+          <label>Categories</label>
+          <input type='text' className='form-control' {...categories} />
+        </div>
+
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea className='form-control' {...content} />
+        </div>
+
+        <button type='submit' className='btn btn-primary'>Submit</button>
+
+      </form>
     );
   }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostNew',
+  fields: ['title', 'categories', 'content']
+})(PostNew);
