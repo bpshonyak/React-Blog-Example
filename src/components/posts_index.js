b09977094e5ffb560bd6d542a8067e4581afc483
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-08 22:37:22 +0900

    promoted PostsIndex to class component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div>List of Blog posts</div>
-  );
+class PostsIndex extends Component {
+
+  // React lifecycle method (only called once on the first render)
+  componentWillMount() {
+    console.log("component mount event!");
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+export default PostsIndex;
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,6 @@ import PostsIndex from './components/posts_index';
 export default (
   <Route path="/" component={App}>
     // PostsIndex compenent available to App compenent on '/' path
-    <IndexRoute component={PostsIndex } />
+    <IndexRoute component={PostsIndex} />
   </Route>
 )
