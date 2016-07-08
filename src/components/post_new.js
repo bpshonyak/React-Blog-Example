b7bbfdbdde07ee83274357379e9492298ae3deaa
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-08 23:45:11 +0900

    added new post form component and route for it

---
+++ src/components/post_new.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+
+  render() {
+    return (
+      <div>New Post Form</div>
+    );
+  }
+}
+
+export default PostNew;
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostNew from './components/post_new';
 
 export default (
   <Route path="/" component={App}>
     // PostsIndex compenent available to App compenent on '/' path
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostNew} />
   </Route>
 )
