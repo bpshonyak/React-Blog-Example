1b6cd381c4d506c214d3e25fa884bd5403c1f32b
Bogdan Pshonyak <bpshonyak@live.com>
2016-07-08 21:24:58 +0900

    installed axios and redux-promise

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    // PostsIndex compenent available to App compenent on '/' path
+    <IndexRoute component={PostsIndex } />
+  </Route>
 )
